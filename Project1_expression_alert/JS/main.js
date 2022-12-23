2009bda6192a62f242f653f209709ce76b460e5a
Mahrukh Ejaz <bobo.zh.cai@gmail.com>
2022-12-23 22:34:12 +0500

    done
    done

---
+++ Project1_expression_alert/JS/main.js
@@ -0,0 +1,73 @@
+// Assignment no 3:
+// alert("Hi!! Welcome to Assignment no 3");
+
+// Assignment no 4:
+// window.alert("Hello peeps!! I am working in the window.alert method.The alert() method displays an alert box with a specified message and an OK button. An alert box is often used if you want to make sure information comes through to the user.");
+
+// Assignment no 5:
+document.write("Hello peeps!!The write() function inserts JavaScript or HTML code into a page. Typically, the write() function is used for testing.");
+
+// Assignment no 6:
+// var x = "A new variable is initialized,Using the keywords var, const, or let, containers that hold values are referred to as variables in JavaScript. A named variable can receive the value of a string.";
+// document.write("<br />")
+// document.write("<br />")
+// document.write(x);
+
+// Assignment no 7:
+// var newValue = "A String being kept in a variable.In JavaScript, variables are called containers because they hold values.";
+// window.alert(newValue);
+
+
+// Assignment no 8:
+// This demonstrates how to use single and double quotes to enclose a string in order to insert an apostrophe using JavaScript. To insert an apostrophe in a string in JavaScript, use double quotes ("").
+// document.write("<br />")
+// document.write("<br />")
+// document.write("He said,Buy some “fresh” chicken today!\"")
+
+// Assignment no 9:
+// document.write("<br />")
+// document.write("<br />")
+// document.write("Little Red Riding Hood is a young girl. " + " She likes to wear a red hood on her head." + " One day, she goes to her grandma’s house."   + " It is in a dark forest.");
+// document.write("<br />")
+// document.write("<br />")
+
+// document.write("key" + "board");
+// document.write("<br>");
+// document.write("tea" + "pot");
+// document.write("<br>")
+
+
+
+// Assignment no 10:
+var Fam = "Victor",
+Mom = "Victoria",Dad = "John",Son = "Brandon";Daughter = "Jasmine";
+document.write("<br>");
+document.write("<br>");
+document.write(Mom);
+document.write("<br>");
+document.write(Dad);
+document.write("<br>");
+
+
+// Assignment no 11:
+document.write("<br>");
+document.write("<br>");
+var Fam = Fam.fontcolor("orange");
+document.write(Fam);
+document.write("<br>");
+var Dad = Dad.fontcolor("blue");
+document.write(Dad);
+var Mom = Mom.fontcolor("pink");
+document.write("<br>");
+document.write(Mom);
+
+
+
+// Assignment no 12:
+// Strings, integers, objects, and other types of data can be stored in variables as containers.
+var a = 15;
+var b = 15;
+document.write("<br>");
+document.write("<br>");
+document.write(a + b);
+
